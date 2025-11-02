3f1525d7459a4312f7957a39967918a37c6d4f95
Jimmy <35855869+jimmy1010ww@users.noreply.github.com>
2025-11-02 16:15:59 +0800

    新增函數參數&更正顯示訊息

--- main.ts
+++ main.ts
@@ -5,9 +5,9 @@ player.onChat("detect", function () {
     偵測寶藏大賽方塊(寶藏座標)
 })
 player.onChat("run", function () {
-    寶藏搜尋大賽(5)
+    寶藏搜尋大賽(5, -300, -400, 61, 63, 70, 250)
 })
-function 寶藏搜尋大賽 (treasure_num: number) {
+function 寶藏搜尋大賽 (treasure_num: number, random_x1: number, random_x2: number, random_y1: number, random_y2: number, random_z1: number, random_z2: number) {
     gameplay.title(mobs.target(ALL_PLAYERS), "尋找寶藏大賽", "快去尋找寶藏!")
     寶藏座標 = []
     player.execute(
@@ -17,7 +17,7 @@ function 寶藏搜尋大賽 (treasure_num: number) {
     "/scoreboard objectives setdisplay sidebar \"找尋寶藏\""
     )
     for (let index = 0; index <= treasure_num - 1; index++) {
-        position = world(randint(-300, -400), randint(61, 63), randint(70, 250))
+        position = world(randint(random_x1, random_x2), randint(random_y1, random_y2), randint(random_z1, random_z2))
         寶藏座標.push(position)
         blocks.place(IRON_BLOCK, position)
         player.execute(
@@ -26,7 +26,7 @@ function 寶藏搜尋大賽 (treasure_num: number) {
     }
     偵測寶藏大賽方塊(寶藏座標)
 }
-function 偵測寶藏大賽方塊 (treasure_pos_list: Position[]) {
+function 偵測寶藏大賽方塊 (treasure_pos_list: any[]) {
     flag = true
     while (flag) {
         flag = false
@@ -40,11 +40,12 @@ function 偵測寶藏大賽方塊 (treasure_pos_list: Position[]) {
                 player.execute(
                 "/scoreboard players set " + "\"§a" + treasure_pos_list[index] + "\"" + "\"找尋寶藏\" 0"
                 )
-                gameplay.title(mobs.target(ALL_PLAYERS), "寶藏被挖掉拉!", "玩家: " + mobs.near(
+                gameplay.title(mobs.target(ALL_PLAYERS), "§e寶藏被挖掉拉!", "§q玩家: " + mobs.near(
                 mobs.target(NEAREST_PLAYER),
                 treasure_pos_list[index],
                 3
                 ) + "拿到寶藏了!")
+                treasure_pos_list.removeAt(index)
             }
         }
     }
