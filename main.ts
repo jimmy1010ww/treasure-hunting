118d5168465d99e047f733161d12589c95ab8fa2
Jimmy <35855869+jimmy1010ww@users.noreply.github.com>
2025-11-02 11:12:36 +0800

    更改可以控制的寶藏數量&獨立出偵測function

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 let 寶藏座標: Position[] = []
 let position: Position = null
+let flag = false
 player.onChat("run", function () {
-    寶藏搜尋大賽()
+    寶藏搜尋大賽(5)
 })
-function 寶藏搜尋大賽 () {
+function 寶藏搜尋大賽 (treasure_num: number) {
     gameplay.title(mobs.target(ALL_PLAYERS), "尋找寶藏大賽", "快去尋找寶藏!")
     寶藏座標 = []
     player.execute(
@@ -12,26 +13,30 @@ function 寶藏搜尋大賽 () {
     player.execute(
     "/scoreboard objectives setdisplay sidebar \"找尋寶藏\""
     )
-    for (let index = 0; index <= 4; index++) {
+    for (let index = 0; index <= treasure_num - 1; index++) {
         position = world(randint(-300, -400), randint(61, 63), randint(70, 250))
         寶藏座標.push(position)
         blocks.place(IRON_BLOCK, position)
         player.execute(
-        "/scoreboard players add " + "\"" + position + "\"" + " \"找尋寶藏\" 1"
+        "/scoreboard players add " + "\"§a" + position + "\"" + " \"§6找尋寶藏\" 1"
         )
     }
+    偵測寶藏大賽方塊(寶藏座標)
 }
-function 偵測寶藏大賽方塊 () {
-    while (true) {
-        for (let index = 0; index <= 4; index++) {
-            if (!(blocks.testForBlock(IRON_BLOCK, 寶藏座標[index]))) {
+function 偵測寶藏大賽方塊 (treasure_pos_list: any[]) {
+    flag = true
+    while (flag) {
+        flag = false
+        for (let index = 0; index <= treasure_pos_list.length; index++) {
+            if (!(blocks.testForBlock(IRON_BLOCK, treasure_pos_list[index]))) {
                 player.execute(
-                "/scoreboard players set " + "\"" + 寶藏座標[index] + "\"" + "\"找尋寶藏\" 0"
+                "/scoreboard players set " + "\"" + treasure_pos_list[index] + "\"" + "\"§6找尋寶藏\" 0"
                 )
             } else {
                 player.execute(
-                "/scoreboard players set " + "\"" + 寶藏座標[index] + "\"" + "\"找尋寶藏\" 1"
+                "/scoreboard players set " + "\"" + treasure_pos_list[index] + "\"" + "\"§6找尋寶藏\" 1"
                 )
+                flag = true
             }
         }
     }
